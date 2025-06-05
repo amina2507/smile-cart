82c7f90bbfe4e4f48814b52f70d7d47830834196
amina2507 <aminaash544@gmail.com>
2025-06-05 20:19:49 +0530

    Added product listing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import { Route, Switch, NavLink } from "react-router-dom";
+import { PageNotFound } from "components/commons";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import Home from "./components/Home";
-import PageNotFound from "./components/PageNotfound";
+import "./App.css";
 import Product from "./components/Product";
+import ProductList from "./components/ProductList";
 
 const App = () => (
-  <>
-    <div className="flex space-x-2">
-      <NavLink exact activeClassName="underline font-bold" to="/">
-        Home
-      </NavLink>
-      <NavLink exact activeClassName="underline font-bold" to="/product">
-        Product
-      </NavLink>
-    </div>
-    <Switch>
-      <Route component={Product} path="/product" />
-      <Route component={Home} path="/" />
-      <Route component={PageNotFound} path="*" />
-    </Switch>
-  </>
+  <Switch>
+    <Route exact component={ProductList} path="/products" />
+    <Route exact component={Product} path="/products/:slug" />
+    <Redirect exact from="/" to="/products" />
+    <Route component={PageNotFound} path="*" />
+  </Switch>
 );
-
 export default App;
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,21 +1,26 @@
 import { useState, useEffect } from "react";
 
-import { Typography, Spinner } from "neetoui";
+import { useParams } from "react-router-dom";
 import { append, isNotNil } from "ramda";
+import { Header, PageLoader, PageNotFound } from "components/commons";
+import { Typography } from "neetoui";
 
 import Carousel from "./Carousel";
 
-import productsApi from "../../apis/products";
+import productsApi from "/apis/products";
 
 const Product = () => {
+  const { slug } = useParams();
+  const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [product, setProduct] = useState({});
 
   const fetchProduct = async () => {
     try {
-      const product = await productsApi.show();
-      setProduct(product);
+      const response = await productsApi.show(slug);
+      setProduct(response);
     } catch (error) {
+      setIsError(true);
       console.log("An error occurred:", error);
     } finally {
       setIsLoading(false);
@@ -25,28 +30,20 @@ const Product = () => {
   useEffect(() => {
     fetchProduct();
   }, []);
-
   const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
 
   const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
 
   if (isLoading) {
-    return (
-      <div className="flex h-screen w-full items-center justify-center">
-        <Spinner />
-      </div>
-    );
+    return <PageLoader />;
   }
 
+  if (isError) return <PageNotFound />;
+
   return (
-    <div className="px-6 pb-6">
-      <div>
-        <Typography className="py-2 text-4xl font-semibold" style="h1">
-          {name}
-        </Typography>
-        <hr className="neeto-ui-border-black border-2" />
-      </div>
+    <>
+      <Header title={name} />
       <div className="mt-16 flex gap-4">
         <div className="w-2/5">
           <div className="flex justify-center gap-16">
@@ -68,7 +65,7 @@ const Product = () => {
           </Typography>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Product;
